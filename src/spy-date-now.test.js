a1240f61efde4e02d5265b0f9313fe1fe1ef8528
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-06-19 21:30:15 +0100

    add date now spy example

--- src/spy-date-now.test.js
+++ src/spy-date-now.test.js
@@ -1,1 +1,13 @@
-test.todo('should create new date');
+// Explanation at https://codewithhugo.com/mocking-the-current-date-in-jest-tests/#spy-on-date-now-and-add-a-mock-implementation
+
+const getNow = () => new Date(Date.now());
+
+test('It should create correct now Date', () => {
+  jest
+    .spyOn(global.Date, 'now')
+    .mockImplementationOnce(() =>
+      new Date('2019-05-14T11:01:58.135Z').valueOf(),
+    )
+
+  expect(getNow()).toEqual(new Date('2019-05-14T11:01:58.135Z'));
+});
