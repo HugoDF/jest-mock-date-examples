6cf75534c8cbf10fc4ea85a8128923eda42445d5
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-06-19 21:27:44 +0100

    add date.now stub example

--- src/stub-date-now.test.js
+++ src/stub-date-now.test.js
@@ -1,1 +1,14 @@
-test.todo('should create new date');
+// Explanation at https://codewithhugo.com/mocking-the-current-date-in-jest-tests/#replacing-date-now-with-a-stub
+
+const literallyJustDateNow = () => Date.now();
+
+test('It should call and return Date.now()', () => {
+  const realDateNow = Date.now.bind(global.Date);
+  const dateNowStub = jest.fn(() => 1530518207007);
+  global.Date.now = dateNowStub;
+
+  expect(literallyJustDateNow()).toBe(1530518207007);
+  expect(dateNowStub).toHaveBeenCalled();
+
+  global.Date.now = realDateNow;
+});
