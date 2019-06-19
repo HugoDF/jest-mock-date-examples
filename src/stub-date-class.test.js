731c28fd4b0ff717a954d49819bca89ce8e530e1
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-06-19 21:31:40 +0100

    add new Date() class stubbing example

--- src/stub-date-class.test.js
+++ src/stub-date-class.test.js
@@ -1,1 +1,24 @@
-test.todo('should create new date');
+// Explanation at https://codewithhugo.com/mocking-the-current-date-in-jest-tests/#mock-the-whole-date-class-with-a-fixed-date-instance
+
+const getCurrentDate = () => new Date();
+let realDate
+
+test('It should create new date', () => {
+  // setup
+  const currentDate = new Date('2019-05-14T11:01:58.135Z');
+  realDate = Date
+  global.Date = class extends Date {
+    constructor(date) {
+      if (date) {
+        return super(date);
+      }
+
+      return currentDate;
+    }
+  }
+
+  expect(getCurrentDate()).toEqual(new Date('2019-05-14T11:01:58.135Z'));
+
+  // cleanup
+  global.Date = realDate
+})
